59481447035081f0e8ded750d6a3ed12ae04ca8f
kgupta03 <karun.gupta@verizonmedia.com>
2021-07-13 16:49:10 +0530

    Completed Chart Section

---
+++ src/dummyData.js
@@ -0,0 +1,50 @@
+export const userData = [
+    {
+      name: 'Jan',
+      "Active User": 4000,
+    },
+    {
+        name: 'Feb',
+        "Active User": 3000,
+    },
+    {
+        name: 'Mar',
+        "Active User": 5000,
+    },
+    {
+        name: 'Apr',
+        "Active User": 3500,
+    },
+    {
+        name: 'May',
+        "Active User": 7000,
+    },
+    {
+        name: 'Jun',
+        "Active User": 5000,
+    },
+    {
+        name: 'Jul',
+        "Active User": 5000,
+    },
+    {
+        name: 'Aug',
+        "Active User": 6000,
+    },
+    {
+        name: 'Sep',
+        "Active User": 6500,
+    },
+    {
+        name: 'Oct',
+        "Active User": 6800,
+    },
+    {
+        name: 'Nov',
+        "Active User": 7000,
+    },
+    {
+        name: 'Dec',
+        "Active User": 8000,
+    },
+  ]
